90ceb5da7163d4784fa8ce7ea0487bdc12176dbc
Sharon-liu-go <innovate72095@gmail.com>
2020-11-15 14:11:28 +0800

    feat: add login page

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,14 +2,14 @@ const express = require("express")
 const router = express.Router()
 
 
-router.get('/login', (req, res => {
-  res, render('login')
-}))
+router.get('/login', (req, res) => {
+  res.render('login')
+})
 
 
-router.get('/register', (req, res => {
+router.get('/register', (req, res) => {
   res.render('register')
-}))
+})
 
 
 
