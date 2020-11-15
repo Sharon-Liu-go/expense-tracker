a23112c595c7a2b89e17d1c43ebc305b03df4e44
Sharon-liu-go <innovate72095@gmail.com>
2020-11-15 15:44:41 +0800

    feat: add expres-session

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const app = express()
 const routes = require("./routes")
 const methodOverride = require("method-override")
 require('./config/mongoose')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+
+
+app.use(session({
+  secret: 'ThisIsDadSecret',
+  resave: false,  //會在每一次與使用者互動後，強制把 session 更新到 session store 裡。
+  saveUninitialized: true //強制將未初始化的 session 存回 session store。未初始化表示這個 session 是新的而且沒有被修改過，例如未登入的使用者的 session。
+}))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
