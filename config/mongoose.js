aef0bc938f51b60536c20bd4d6831542a9d65cff
Sharon-liu-go <innovate72095@gmail.com>
2020-12-09 15:51:37 +0800

    modify seed script

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/todo-list'
 mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
 
 const db = mongoose.connection
