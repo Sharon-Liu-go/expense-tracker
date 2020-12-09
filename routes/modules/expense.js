52e6d5625ecea5ae72dad9fa82d1688a6d30a789
Sharon-liu-go <innovate72095@gmail.com>
2020-12-09 15:15:45 +0800

    modify router/expense/put:id and sent some condition limit for input

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -75,24 +75,60 @@ router.get('/edit/:id', (req, res) => {
 })
 
 router.put('/:id', (req, res) => {
-  let categoryChosen = req.body.category
-  const id = req.params.id
+  let { name, date, category, amount, merchant } = req.body
+  let yearFilledIn = date.slice(0, 4)
   let icon = ""
-  Category.findOne({ category: categoryChosen })
-    .lean()
-    .then(categoryItem => {
-      icon = categoryItem.icon
-      console.log(`icon:${icon}`)
-      return Record.findById(id)
-        .then(record => {
-          record = Object.assign(record, req.body, { icon })
-          return record.save()
-        })
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
-    })
+  const id = req.params.id
+  console.log(req.body)
 
+  Promise.all([
+    Year.find({ year: yearFilledIn }).lean(),
+    Category.findOne({ category: category }).lean(),
+    Record.findById(id)
+  ])
+    .then(([yearItem, categoryItem, record]) => {
+
+      if (!name || !date || category == "Choose..." || !amount) {
+        req.flash('warning_msg', '＊為必填欄位')
+        return res.redirect(`/expense/edit/${id}`)
+      } else if (date.length !== 10 && date.indexOf(" ")) {
+        req.flash('warning_msg', 'date欄位請依yyyy/mm/dd格式')
+        return res.redirect(`/expense/edit/${id}`)
+      } else {
+
+        //將未建立過的年份寫入year的資料庫
+        if (yearItem.length === 0) {
+          Year.create(Object.assign({ year: yearFilledIn }))
+        }
+
+        icon = categoryItem.icon
+
+        record = Object.assign(record, req.body, { icon })
+        record.save()
+        return res.redirect('/')
+
+      }
+    }).catch(error => console.log(error))
 })
+
+//   let categoryChosen = req.body.category
+//   const id = req.params.id
+//   let icon = ""
+//   Category.findOne({ category: categoryChosen })
+//     .lean()
+//     .then(categoryItem => {
+//       icon = categoryItem.icon
+//       console.log(`icon:${icon}`)
+//       return Record.findById(id)
+//         .then(record => {
+//           record = Object.assign(record, req.body, { icon })
+//           return record.save()
+//         })
+//         .then(() => res.redirect('/'))
+//         .catch(error => console.log(error))
+//     })
+
+// })
 
 router.delete('/:id', (req, res) => {
   const id = req.params.id
